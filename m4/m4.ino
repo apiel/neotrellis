7ed30ff2973f8d56ff0139f164260640b2d52b4a
Alexandre Piel <alexandre.piel@zooplus.com>
2023-12-19 21:21:42 +0100

    cleanup

--- m4/m4.ino
+++ m4/m4.ino
@@ -15,37 +15,32 @@ Adafruit_NeoTrellisM4 trellisM4 = Adafruit_NeoTrellisM4();
 
 bool initialised = true;
 
-struct Rgb
-{
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-} colors[] = {
-    {0xff, 0xff, 0xff}, // #ffffff
-    {0x37, 0x61, 0xa1}, // #3761a1
-    {0x37, 0x91, 0xa1}, // #3791a1
-    {0x00, 0xFF, 0xEF}, // #00ffef
-    {0x00, 0xb3, 0x00}, // #00b300
-    {0x9d, 0xfe, 0x86}, // #9dfe86
-    {0xff, 0x8d, 0x99}, // #ff8d99
-    {0xff, 0x00, 0x00}, // #ff0000
-    {0xff, 0x00, 0x77}, // #ff0077
-    {0xff, 0x77, 0x00}, // #ff7700
-    {0xd0, 0x9f, 0xf5}, // #d09ff5
-    {0xa1, 0x9f, 0xfc}, // #a19ffc
-    {0x80, 0x00, 0x80}, // #800080
-    {0xa7, 0x4b, 0x4b}, // #a7194b
-    {0xff, 0xff, 0x00}, // #ffff00
-    {0x96, 0x4b, 0x00}, // #964B00
+uint32_t colors[] = {
+    0xffffff,
+    0x3761a1,
+    0x3791a1,
+    0x00ffef,
+    0x00b300,
+    0x9dfe86,
+    0xff8d99,
+    0xff0000,
+    0xff0077,
+    0xff7700,
+    0xd09ff5,
+    0xa19ffc,
+    0x800080,
+    0xa7194b,
+    0xffff00,
+    0x964B00,
 };
 
-const uint8_t colorCount = sizeof(colors) / sizeof(Rgb);
+const uint8_t colorCount = sizeof(colors) / sizeof(uint32_t);
 const uint8_t maxColor = colorCount * 10;
 const float brightnessRatio = 255.0 / 10.0;
 
 struct Color
 {
-    Rgb rgb;
+    uint32_t value;
     uint8_t brightness;
 };
 
@@ -53,19 +48,13 @@ Color getColor(uint8_t color)
 {
     if (color > maxColor)
     {
-        return {
-            .rgb = {0x00, 0x00, 0x00},
-            .brightness = 0,
-        };
+        return {.value = 0, .brightness = 0};
     }
 
     uint8_t colorIndex = color / 10;
     uint8_t brighness = (color % 10) * brightnessRatio + 5;
 
-    return {
-        .rgb = colors[colorIndex],
-        .brightness = brighness,
-    };
+    return {.value = colors[colorIndex], .brightness = brighness};
 }
 
 uint8_t counter = 0;
@@ -75,12 +64,9 @@ TrellisCallback blink(keyEvent evt)
     if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING)
     {
         Color color = getColor(counter++);
-
-        Serial.printf("color: r %d g %d b %d  brightness %d\n", color.rgb.r, color.rgb.g, color.rgb.b, color.brightness);
-
-        uint32_t c = trellis.pixels.Color(color.rgb.r, color.rgb.g, color.rgb.b);
+        Serial.printf("color: rgb #%x  brightness %d\n", color.value, color.brightness);
         trellis.pixels.setBrightness(color.brightness);
-        trellis.pixels.setPixelColor(evt.bit.NUM, c);
+        trellis.pixels.setPixelColor(evt.bit.NUM, color.value);
     }
     else if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING)
     {
@@ -95,18 +81,13 @@ TrellisCallback blink(keyEvent evt)
 void setup()
 {
     Serial.begin(115200);
+
+    trellisM4.begin();
+
     if (!trellis.begin())
     {
         initialised = false;
     }
-
-    trellisM4.begin();
-    // if (!)
-    // {
-    //     initialised = false;
-    // }
-
-    // Serial.println("basic keypad test!");
 
     // activate all keys and set callbacks
     for (int i = 0; i < NEO_TRELLIS_NUM_KEYS; i++)
@@ -130,7 +111,12 @@ void loop()
         if (e.bit.EVENT == KEY_JUST_PRESSED)
         {
             Serial.println(" pressed");
-            trellisM4.setPixelColor(e.bit.KEY, 0xFFFFFF);
+            // trellisM4.setPixelColor(e.bit.KEY, 0xFFFFFF);
+
+            Color color = getColor(counter++);
+            Serial.printf("color: rgb #%x  brightness %d\n", color.value, color.brightness);
+            trellisM4.setBrightness(color.brightness);
+            trellisM4.setPixelColor(e.bit.KEY, color.value);
         }
         else if (e.bit.EVENT == KEY_JUST_RELEASED)
         {
